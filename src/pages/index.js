76a547eafb83bca399fbc49d4436dc8fed8a9ee7
Hugues Tennier <huguestennier@gmail.com>
2020-07-23 12:24:59 -0400

    Since we're using colored background, we can remove the SVG preview and load the low res image like on the product details page

--- src/pages/index.js
+++ src/pages/index.js
@@ -21,12 +21,12 @@ export const query = graphql`
               localFile {
                 childImageSharp {
                   fluid(
-                    maxWidth: 1600
+                    maxWidth: 900
                     maxHeight: 900
                     fit: COVER
                     cropFocus: CENTER
                   ) {
-                    ...GatsbyImageSharpFluid_withWebp_tracedSVG
+                    ...GatsbyImageSharpFluid
                   }
                 }
               }
