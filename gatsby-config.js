1f8bc7ec58beac7d0c898b618d4d0735689c1ba1
Hugues Tennier <huguestennier@gmail.com>
2020-07-28 17:42:44 -0400

    Add gatsby helmet plugin

--- gatsby-config.js
+++ gatsby-config.js
@@ -7,6 +7,7 @@ module.exports = {
     "gatsby-transformer-sharp",
     "gatsby-plugin-sharp",
     "gatsby-plugin-sitemap",
+    "gatsby-plugin-react-helmet",
     {
       resolve: "gatsby-source-filesystem",
       options: {
