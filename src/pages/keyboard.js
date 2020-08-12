9ac681ef4591fac69019b22b4b9d8db8d3f42b24
Jason Lengstorf <jason@lengstorf.com>
2020-08-11 17:23:24 -0700

    fix: switch to single collection query

--- src/pages/keyboard.js
+++ src/pages/keyboard.js
@@ -7,60 +7,53 @@ import SEO from '../components/seo';
 
 export const query = graphql`
   {
-    allShopifyCollection(filter: { handle: { eq: "netlify-exclusive" } }) {
-      nodes {
-        descriptionHtml
-        handle
+    shopifyCollection(handle: { eq: "netlify-exclusive" }) {
+      descriptionHtml
+      handle
+      title
+      products {
+        id
         title
-        products {
-          id
+        slug
+        productType
+        description
+        variants {
+          shopifyId
           title
-          slug
-          productType
-          description
-          variants {
-            shopifyId
-            title
-            availableForSale
-            image {
-              localFile {
-                childImageSharp {
-                  fluid(
-                    maxWidth: 900
-                    maxHeight: 900
-                    fit: COVER
-                    cropFocus: CENTER
-                  ) {
-                    ...GatsbyImageSharpFluid
-                  }
+          availableForSale
+          image {
+            localFile {
+              childImageSharp {
+                fluid(
+                  maxWidth: 900
+                  maxHeight: 900
+                  fit: COVER
+                  cropFocus: CENTER
+                ) {
+                  ...GatsbyImageSharpFluid
                 }
               }
             }
-            priceV2 {
-              amount
-              currencyCode
-            }
-            sku
           }
+          priceV2 {
+            amount
+            currencyCode
+          }
+          sku
         }
       }
     }
   }
 `;
 
-const exclusiveCollection = 'netlify-exclusive';
-
 export default ({ data }) => {
   const [secret, setSecret] = useState();
-  const exclusive = data.allShopifyCollection.nodes.filter((node) => {
-    return node.handle === exclusiveCollection;
-  });
 
   return (
     <Layout home>
       <SEO />
       {secret ? (
-        <CollectionListings collection={exclusive[0]} />
+        <CollectionListings collection={data.shopifyCollection} />
       ) : (
         <PasswordLock handleCorrectPassword={() => setSecret(true)} />
       )}
